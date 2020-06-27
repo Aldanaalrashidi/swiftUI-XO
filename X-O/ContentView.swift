f0d741e4973c4e9d82bb5e4b0480a71742764b84
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2020-06-27 16:39:41 +0300

    Initial Commit

---
+++ X-O/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X-O
+//
+//  Created by Retaj Al-Otaibi on 6/27/20.
+//  Copyright © 2020 Retaj Al-Otaibi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
